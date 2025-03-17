a442394e16ea51369b5d942f60e66890f7eed205
harukiobata <barukan0913@gmail.com>
2025-03-18 00:28:36 +0900

    add/roomのmodel,controllerの作成,routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homepage/index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms
   root 'homepage#index'
   get 'users/account', to: 'users#account', as: 'account_user'
   get 'users/profile', to: 'users#profile', as: 'profile_user'
